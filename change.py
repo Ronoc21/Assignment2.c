53fc7c4762d556dd3634c4aa28a3749c31759b8c
Conor <conor.okeefe.2121@gmail.com>
2021-04-07 22:50:13 -0700

    Converts a number less than 100 into appropriate change (USD)

--- change.py
+++ change.py
@@ -7,7 +7,7 @@ N = 5
 P = 1
 print("Please enter an amount in cents less than a dollar.")
 num_1 = int(input())
-print("Your change will be")
+print("Your change will be:")
 print("Q:", num_1 // Q)
 print("D:", num_1 % Q // D)
 print("N:", num_1 % Q % D // N)
